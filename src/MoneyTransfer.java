93aaed51c4153e5456ff35874e8aa1af92331888
Sharipovaa <alfya.yanbekova28@gmail.com>
2021-12-21 20:23:22 +0500

    добавлено

---
+++ src/MoneyTransfer.java
@@ -0,0 +1,8 @@
+public class MoneyTransfer {
+    public static void main(String[] args) {
+        int chet = 2000000000;
+        int perevod = 500000000;
+        int sum = chet + perevod;
+        System.out.println(sum);
+    }
+}
